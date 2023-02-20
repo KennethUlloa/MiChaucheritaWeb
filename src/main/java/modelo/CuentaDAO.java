fe17bfa58187825403d13416931b54e0b9992165
EmilioMontalvo <80224370+EmilioMontalvo@users.noreply.github.com>
2023-02-19 20:48:15 -0500

    Cuenta DAO

--- src/main/java/modelo/CuentaDAO.java
+++ src/main/java/modelo/CuentaDAO.java
@@ -3,9 +3,9 @@ package modelo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CuentaDAO {
+public class CuentaDAO implements ICuentaDAO {
 	private static List<Cuenta> cuentas = null;
-	
+	/*
 	public Cuenta getCuenta(int id) {
 		for (Cuenta cuenta : getCuentas()) {
 			if(cuenta.getNumeroCuenta()==id) {
@@ -14,7 +14,7 @@ public class CuentaDAO {
 		}
 		return null;
 	}
-
+	/*
 	public List<Cuenta> getCuentas() {
 		if(cuentas == null) {
 			cuentas = new ArrayList<Cuenta>();
@@ -25,6 +25,47 @@ public class CuentaDAO {
 			cuentas.add(new CuentaIngresoEgreso(4,"Efectivo"));
 		}
 		return cuentas;
+	}*/
+
+	@Override
+	public void create(Cuenta object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Cuenta getBy(Integer id) {
+		for (Cuenta cuenta : getAll()) {
+			if(cuenta.getNumeroCuenta()==id) {
+				return cuenta;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public List<Cuenta> getAll() {
+		if(cuentas == null) {
+			cuentas = new ArrayList<Cuenta>();
+			cuentas.add(new CuentaIngresos(1,"Nómina"));
+			cuentas.add(new CuentaEgresos(2,"Regalo"));
+			cuentas.add(new CuentaEgresos(5,"Universidad"));
+			cuentas.add(new CuentaIngresoEgreso(3,"Banco"));
+			cuentas.add(new CuentaIngresoEgreso(4,"Efectivo"));
+		}
+		return cuentas;
+	}
+
+	@Override
+	public void update(Cuenta object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
 	}
 
 		
